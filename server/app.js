746ccc6c2812bca53d637e02e21a75029b8f8378
Priyanka7011 <pk3201134@gmail.com>
2021-07-14 22:07:54 +0530

    structure

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const { check, validationResult } = require('express-validator')
+const mongoose = require('mongoose')
+const app = express()
+const port = 5000
+
+//const Alumni = require('./models/alumniModel')
+
+
+dotenv.config({path:'./config.env'})
+
+require('./db/conn') 
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+//app.use(require('./router/auth'));
+
+const middleware=(req,res,next)=>{
+    console.log(`Hello my middleware`);
+    next();
+}
+
+
+
+
+ 
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`)
+})
