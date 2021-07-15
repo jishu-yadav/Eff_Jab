d75137b3f6773ccdce95c31e68a450f13d74c658
Priyanka7011 <pk3201134@gmail.com>
2021-07-15 13:13:00 +0530

    navbar design

--- eff-jab/src/navbar/navbar.js
+++ eff-jab/src/navbar/navbar.js
@@ -1,7 +1,7 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+//import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import React,{useState} from 'react';
 import {Route,Link} from 'react-router-dom';
-import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome'
+//import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
 import { Navbar,NavDropdown,FormControl,Button,Nav,Form } from 'react-bootstrap';
 //import './navbar.css'
 import $ from 'jquery'
@@ -12,9 +12,9 @@ const NavBar=()=>{
     return(
 
       <div>
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" style={{'borderRadius':'0px'}}>
           <Navbar.Brand href="/">EFF_JAB</Navbar.Brand>
-          <Nav className="me-auto">
+          <Nav className="ml-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/register">Register</Nav.Link>
             <Nav.Link href="/book">Book Appointment</Nav.Link>
