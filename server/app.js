d3df4172e91b97e561f1cafae96672542de21704
jishu-yadav <jishuyadavcute88@gmail.com>
2021-07-15 14:06:08 +0530

    vaccination_s

--- server/app.js
+++ server/app.js
@@ -3,28 +3,27 @@ const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const { check, validationResult } = require('express-validator')
 const mongoose = require('mongoose')
-const app = express()
+const app = express();
 const port = 5000
 
 //const Alumni = require('./models/alumniModel')
 
-
 dotenv.config({path:'./config.env'})
-
 require('./db/conn') 
 
 //Middleware
 app.use(express.json())
+//app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}))
+app.use(require('./router/auth'));
+
+
 //app.use(require('./router/auth'));
 
 const middleware=(req,res,next)=>{
     console.log(`Hello my middleware`);
     next();
 }
-
-
-
 
  
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
