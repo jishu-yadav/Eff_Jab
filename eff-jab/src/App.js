995494646ea6980134bd03546d825e5f89486c5f
jishu-yadav <jishuyadavcute88@gmail.com>
2021-07-15 14:01:37 +0530

    Vaccination_Center

--- eff-jab/src/App.js
+++ eff-jab/src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import CenterRegister from "./components/CenterRegister";
 function App() {
   return (
     
       <BrowserRouter>
+      
          <Switch>
+       
            <Route path='/' exact>
-             
+           <h1>Hello! home page </h1>
            </Route>
-         
+           <Route path='/register' exact>
+              <CenterRegister />
+           </Route>
+
          </Switch>
       </BrowserRouter>
       
