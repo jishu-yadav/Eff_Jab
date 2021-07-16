8fe58c7cd2823653b2b3cff9e5e79e1b8405a133
Aditi-Roy-coder <aditiroy.21.ar@gmail.com>
2021-07-16 11:26:38 +0530

    navbar

--- eff-jab/src/navbar/navbar.js
+++ eff-jab/src/navbar/navbar.js
@@ -16,7 +16,7 @@ const NavBar=()=>{
           <Navbar.Brand href="/">EFF_JAB</Navbar.Brand>
           <Nav className="ml-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            
+            <Nav.Link href="/centerregister">Center</Nav.Link>
             <Nav.Link href="/community">Community</Nav.Link>
             <Nav.Link href="/upvote">Upvote</Nav.Link>
             <Nav.Link href="https://covid19selfdiagnosis.herokuapp.com/">Self Diagnosis</Nav.Link>
